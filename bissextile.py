543a806947eddd38e282cef45e08a1d1a9fad43d
Nils Layet <nils@epita.fr>
2017-08-04 16:50:11 +0200

    commit

--- bissextile.py
+++ bissextile.py
@@ -1,18 +1,17 @@
-r = "est bissextile"
-w = "n'est pas bissextile"
+def printYear(y, isLeap):
+    y = str(y)
+    if isLeap:
+        print(y + " is a leap year")
+    else:
+        print(y + " is not a leap year")
 
 def b(a):
-
-
-	if a % 4 == 0 and a % 100 != 0:
-			print(a, r)
-
-	elif a % 400 == 0 :
-			print(a, r)
-
-	else:
-		print(a, w)
-
+    if a % 4 == 0 and a % 100 != 0:
+        printYear(a, True)
+    elif a % 400 == 0:
+        printYear(a, True)
+    else:
+        printYear(a, False)
 
 b(2009)
 b(2092)
