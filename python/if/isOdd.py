2f8f4970cc88f6b44cc6e0d364a77663b7264b33
Bopzor <violaine.ballandin@gmail.com>
2017-08-10 16:45:08 +0200

    exo

---
+++ python/if/isOdd.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number : "))
+isOdd = True
+
+def iO(n):
+    if n % 2 != 0:
+        print("True")
+    else:
+        print("False")
---
+++ python/if/isPositive.py
@@ -0,0 +1,8 @@
+def isPositive(n):
+    if n > 0:
+        print("True")
+    else:
+        print("False")
+
+n = int(input("Enter a number : "))
+isPositive(n)
