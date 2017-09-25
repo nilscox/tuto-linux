a5ebdb45af9ce7fba62cedd0fb73b0ead625e288
Bopzor <violaine.ballandin@gmail.com>
2017-09-25 14:41:56 +0200

    modif

---
+++ python/irc-bot/lol.py
@@ -0,0 +1,9 @@
+import random
+
+jokelist = [
+    "Prout", "Bite", "Chatte", "Cul", "DP", "Trololololo, Concaténer"
+]
+
+def lol(args):
+    return random.choice(jokelist)
+
--- python/irc-bot/moreorless.py
+++ python/irc-bot/moreorless.py
@@ -3,6 +3,7 @@ from random import randint
 
 secret_nb = 0
 
+
 def moreorless(args):
 
     global secret_nb
@@ -12,12 +13,12 @@ def moreorless(args):
         return "Secret Number is defined."
 
     elif args[0] == "answer":
-        while int(args[1]) != secret_nb:
-            if int(args[1]) > secret_nb:
-                return "Less !"
-            elif int(args[1]) < secret_nb:
-                return "More !"
-        return "You win !" + str(secret_nb) + "is the Secret Number !"
+        if int(args[1]) > secret_nb:
+            return "Less !"
+        elif int(args[1]) < secret_nb:
+            return "More !"
+        else:
+            return "You win !" + str(secret_nb) + "is the Secret Number !"
 
     else:
         return "You must enter 'start' if you want to begin or 'answer' if you want to continue."
