dff58469a76cf033e95f822f2f188b49d4248d83
Bopzor <violaine.ballandin@gmail.com>
2017-09-25 15:00:38 +0200

    modif

--- python/irc-bot/moreorless.py
+++ python/irc-bot/moreorless.py
@@ -4,22 +4,31 @@ from random import randint
 secret_nb = 0
 
 
-def moreorless(args):
+def start(args):
 
     global secret_nb
 
+    secret_nb = randint(0, 999)
+    return "Secret Number is defined."
+
+
+def answer(args):
+
+    global secret_nb
+
+    if int(args[1]) > secret_nb:
+        return "Less !"
+    elif int(args[1]) < secret_nb:
+        return "More !"
+    else:
+        return "You win ! " + str(secret_nb) + " is the Secret Number !"
+
+
+def moreorless(args):
+
     if args[0] == "start":
-        secret_nb = randint(0, 999)
-        return "Secret Number is defined."
-
+        return start(args)
     elif args[0] == "answer":
-        if int(args[1]) > secret_nb:
-            return "Less !"
-        elif int(args[1]) < secret_nb:
-            return "More !"
-        else:
-            return "You win !" + str(secret_nb) + " is the Secret Number !"
-
+        return answer(args)
     else:
         return "You must enter 'start' if you want to begin or 'answer' if you want to continue."
-
