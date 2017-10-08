d21956df4012575df3e489716c26750fde155acf
Nils Layet <nils@epita.fr>
2017-10-08 14:04:39 +0000

    remove debug display

--- python/bubble/game.py
+++ python/bubble/game.py
@@ -1,4 +1,3 @@
-from calculations import cell_box
 from cursor import Cursor
 from grid import Grid
 
@@ -8,9 +7,6 @@ class Game:
     def __init__(self, canvas):
         self.cursor = Cursor(canvas)
         self.grid = Grid(canvas)
-
-        for c in self.grid.cells[4][5].get_adjacent(self.grid.cells):
-            c.square = canvas.create_rectangle(*cell_box(c.position), fill='blue')
 
     def update(self):
         self.grid.update()
