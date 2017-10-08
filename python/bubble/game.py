3d21b7f32c8e3874d77fe9899f3ae195a9703dbf
Nils Layet <nils@epita.fr>
2017-10-08 18:59:34 +0000

    spaw a line sometimes...

--- python/bubble/game.py
+++ python/bubble/game.py
@@ -1,3 +1,6 @@
+from time import time
+
+from constants import GRID_LINE_SPAWN_TIME
 from cursor import Cursor
 from grid import Grid
 
@@ -7,6 +10,14 @@ class Game:
     def __init__(self, canvas):
         self.cursor = Cursor(canvas)
         self.grid = Grid(canvas)
+        self.last_line_spawn = time()
 
     def update(self):
+        now = time()
+        diff = now - self.last_line_spawn
+
+        if diff >= GRID_LINE_SPAWN_TIME:
+            self.grid.spawn_line()
+            self.last_line_spawn = now
+
         self.grid.update()
