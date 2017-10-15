08f6e77385f98cf46486aa7281253a46e8fb6a0d
Bopzor <violaine.ballandin@gmail.com>
2017-10-15 22:58:09 +0200

    win points with combos

--- python/bubble/scoremanager.py
+++ python/bubble/scoremanager.py
@@ -4,6 +4,8 @@ import events
 class ScoreManager:
 
     def __init__(self):
+        self.combo_points = 0
+
         events.subscribe('pop', self.on_pop)
         events.subscribe('detach', self.on_detach)
 
@@ -11,7 +13,13 @@ class ScoreManager:
         events.publish('win_points', points)
 
     def on_pop(self, bubbles):
-        self.win_points(len(bubbles))
+        nbubbles = len(bubbles)
+
+        if nbubbles == 0:
+            self.combo_points = 0
+        else:
+            self.combo_points += nbubbles
+            self.win_points(self.combo_points)
 
     def on_detach(self, bubbles):
         self.win_points(sum(range(len(bubbles) + 1)))
