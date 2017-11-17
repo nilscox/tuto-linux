ee4a98c75de04c25dc74cc4603d59487a9dd4982
Bopzor <violaine.ballandin@gmail.com>
2017-11-17 13:47:40 +0100

    add error code 1 sys.exit

--- python/ytshare/ytshare.py
+++ python/ytshare/ytshare.py
@@ -1,20 +1,22 @@
 import sys
 
 
+def save_url(url):
+    p = open('playlist.txt', 'a')
+    p.write(url + "\n")
+    p.close()
+
+
 def main():
 
     tab = sys.argv
-    url = sys.argv[1]
-
-    def save_url(url):
-        p = open('playlist.txt', 'a')
-        p.write(url + "\n")
-        p.close()
+    url = tab[1]
 
     if len(tab) == 2:
         save_url(url)
     else:
         print("usage:", tab[0], "<youtube-url>")
+        sys.exit(1)
 
 
 if __name__ == '__main__':
