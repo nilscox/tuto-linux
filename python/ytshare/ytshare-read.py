36b0618508025924299724a9d63b355a7f74792a
Bopzor <violaine.ballandin@gmail.com>
2017-11-17 16:49:38 +0100

    add for loop to print urls

--- python/ytshare/ytshare-read.py
+++ python/ytshare/ytshare-read.py
@@ -1,23 +1,29 @@
 import sys
+import os
 
 
 def read_urls():
 
-    url =[]
-    p = open('playlist.txt', "r")
-    line = p.readline()
-    while line:
-        url.append(line)
-        line = p.readline()
+    urls = []
 
-    return url
+    if not os.path.isfile('playlist.txt'):
+        return []
+
+    else:
+        p = open('playlist.txt', "r")
+        line = p.readline()
+        while line:
+            urls.append(line)
+            line = p.readline()
+
+        return urls
 
 
 def main():
 
     if len(sys.argv) == 1:
-        read_urls()
-        print(read_urls())
+        for url in read_urls():
+            print(url)
     else:
         print("usage:", sys.argv[0])
         sys.exit(1)
