a199f73ce92166b33b37ffcca4d612785aa898ef
Bopzor <violaine.ballandin@gmail.com>
2017-11-17 15:49:10 +0100

    add read_urls func

--- python/ytshare/ytshare-read.py
+++ python/ytshare/ytshare-read.py
@@ -1,13 +1,27 @@
 import sys
 
 
-p = open('playlist.txt', "r")
-urls_list = p.read()
+def read_urls():
 
-if len(sys.argv) == 1:
-    p.read()
-    print(urls_list)
-    p.close()
-else:
-    print("usage:", sys.argv[0])
-    sys.exit(1)
+    url =[]
+    p = open('playlist.txt', "r")
+    line = p.readline()
+    while line:
+        url.append(line)
+        line = p.readline()
+
+    return url
+
+
+def main():
+
+    if len(sys.argv) == 1:
+        read_urls()
+        print(read_urls())
+    else:
+        print("usage:", sys.argv[0])
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
