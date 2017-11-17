8248b900318cc3f9a206d276d722d974d3c386af
Bopzor <violaine.ballandin@gmail.com>
2017-11-17 12:52:07 +0100

    wrapped in a main func

--- python/ytshare/ytshare.py
+++ python/ytshare/ytshare.py
@@ -1,19 +1,21 @@
 import sys
 
 
-tab = sys.argv
-url = sys.argv[1]
+def main():
+
+    tab = sys.argv
+    url = sys.argv[1]
+
+    def save_url(url):
+        p = open('playlist.txt', 'a')
+        p.write(url + "\n")
+        p.close()
+
+    if len(tab) == 2:
+        save_url(url)
+    else:
+        print("usage:", tab[0], "<youtube-url>")
 
 
-def save_url(url):
-    p = open('playlist.txt', 'a')
-    p.write(url + "\n")
-    p.close()
-
-
-if len(tab) == 2:
-    save_url(url)
-else:
-    print("usage:", tab[0], "<youtube-url>")
-
-
+if __name__ == '__main__':
+    main()
