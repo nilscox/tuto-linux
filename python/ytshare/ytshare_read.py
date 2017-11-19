168a8e3918dea6d58919c78ec9442822a71b990e
Bopzor <violaine.ballandin@gmail.com>
2017-11-19 18:52:40 +0100

    add html

--- python/ytshare/ytshare_read.py
+++ python/ytshare/ytshare_read.py
@@ -19,13 +19,34 @@ def read_urls():
         return urls
 
 
+def get_html():
+    html = '<!DOCTYPE html>'
+    html += '<html>'
+    html += '<head>'
+    html += '<title>playlist</title>'
+    html += '</head>'
+    html += '<body>'
+
+    for url in read_urls():
+        html += '<p>'
+        html += '<iframe width="560" height="315" src="'
+        html += url[:-1]
+        html += '" frameborder="0" gesture="media" allowfullscreen></iframe>'
+        html += '</p>'
+
+    html += '</body>'
+    html += '</html>'
+
+    return html
+
+
 def main():
 
     if len(sys.argv) == 1:
         for url in read_urls():
             print(url)
     else:
-        print("usage:", sys.argv[0])
+        print('usage:', sys.argv[0])
         sys.exit(1)
 
 
