82f11f5a0f5a33d9fcea81fbcdc1ef8e1c848a8d
Nils Layet <nils@epita.fr>
2017-11-19 21:44:35 +0100

    handle request function

--- python/ytshare/httpd.py
+++ python/ytshare/httpd.py
@@ -7,12 +7,31 @@ from ytshare_read import get_html
 PORT = 4269
 
 class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write(get_html().encode())
+    def handle_request(self, func, *args, **kwargs):
+        status = None
+        content = None
+        content_type = None
 
+        try:
+            status = 200
+            content_type = 'text/html'
+            content = func(*args, **kwargs)
+        except e:
+            status = 500
+            content_type = 'text/plain'
+            content = e.msg()
+
+        content = content.encode()
+
+        self.send_response(status)
+        self.send_header('Content-type', content_type)
+        self.send_header('Content-length', len(content))
+        self.end_headers()
+
+        self.wfile.write(content)
+
+    def do_GET(self):
+        return self.handle_request(get_html)
 
 class MyTCPServer(socketserver.TCPServer):
     def __init__(self, host, ip):
