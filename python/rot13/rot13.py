7b95eb67143bec00d50c6af5b224679d354ed07d
Bopzor <violaine.ballandin@gmail.com>
2017-11-24 16:26:23 +0100

    rot13(letter), func that do a rotation by 13

--- python/rot13/rot13.py
+++ python/rot13/rot13.py
@@ -1,51 +1,25 @@
-import sys
-import os
-from shutil import copyfile
+def rot13(letter):
 
+    up_a = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    low_a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', ' w', 'x', 'y', 'z']
 
-def main():
-
-    def read_file():
-
-        if not os.path.isfile(sys.argv[1]):
-            print('There is no file name ', sys.argv[1])
-            sys.exit(1)
-
-        file = open(sys.argv[1], 'r+')
-        while True:
-            letter = file.read(1)
-
-            file.close()
-            return letter
-
-    def copy_file():
-        name = sys.argv[1]
-        rot13_name = "rot13_" + name
-
-        read_file()
-        copyfile(name, rot13_name)
-        return rot13_name
-
-    def switch_letter():
-        alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-        cap_alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-        for letter in read_file():
-            if letter in alpha:
-                x = alpha.index(letter)
-                new_letter = letter.replace(letter, alpha[x + 13])
-                rot13 = open(str(copy_file()), "w")
-                rot13.write(new_letter)
-            elif letter in cap_alpha:
-                y = cap_alpha.index(letter)
-                new_letter = letter.replace(letter, cap_alpha[y + 13])
-                rot13 = open(str(copy_file()), "w")
-                rot13.write(new_letter)
-
-
-    switch_letter()
-
-if __name__ == '__main__':
-    main()
-
-
+    if letter in up_a:
+        x = int(up_a.index(letter))
+        if x < 13:
+            new_letter = up_a[x + 13]
+            return new_letter
+        else:
+            new_letter = up_a[x - 13]
+            return new_letter
+    elif letter in low_a:
+        y = int(low_a.index(letter))
+        if y < 13:
+            new_letter = low_a[y + 13]
+            return new_letter
+        else:
+            new_letter = low_a[y - 13]
+            return new_letter
+    else:
+        return letter
