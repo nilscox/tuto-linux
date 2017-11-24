45b16d5b7d1aea9798a2599224758bb75019d37d
Bopzor <violaine.ballandin@gmail.com>
2017-11-24 15:04:24 +0100

    correct copy file_name and add capital letters

--- python/rot13/rot13.py
+++ python/rot13/rot13.py
@@ -14,30 +14,38 @@ def main():
         file = open(sys.argv[1], 'r+')
         while True:
             letter = file.read(1)
+
             file.close()
             return letter
 
-
-    def switch_letter(letter):
-        alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-#        cap_alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    def copy_file():
         name = sys.argv[1]
-        rot13_name = "rot13" + name
+        rot13_name = "rot13_" + name
 
         read_file()
         copyfile(name, rot13_name)
+        return rot13_name
 
-        x = alpha.index(letter)
-#        y = cap_alpha.index(letter)
+    def switch_letter():
+        alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        cap_alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-        if letter in alpha:
-            letter.replace(letter, alpha[x + 13])
- #       elif letter in cap_alpha:
-#           letter.replace(letter, cap_alpha[y + 13])
+        for letter in read_file():
+            if letter in alpha:
+                x = alpha.index(letter)
+                new_letter = letter.replace(letter, alpha[x + 13])
+                rot13 = open(str(copy_file()), "w")
+                rot13.write(new_letter)
+            elif letter in cap_alpha:
+                y = cap_alpha.index(letter)
+                new_letter = letter.replace(letter, cap_alpha[y + 13])
+                rot13 = open(str(copy_file()), "w")
+                rot13.write(new_letter)
 
-        print(letter.replace(letter, alpha[x + 13]))
 
-    switch_letter("a")
+    switch_letter()
 
 if __name__ == '__main__':
     main()
+
+
