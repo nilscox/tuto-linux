0ae0ed9eb5b1af02dc62e04abc5551edb4951b0f
Bopzor <violaine.ballandin@gmail.com>
2017-11-26 23:10:46 +0100

    refactor rot13.py

--- python/rot13/rot13.py
+++ python/rot13/rot13.py
@@ -4,7 +4,6 @@ import re
 
 
 def rot13(letter):
-
     up_a = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     low_a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
@@ -30,49 +29,56 @@ def rot13(letter):
         return letter
 
 
-def create_new_file(old_file):
-
-    match = re.match("((.+)\.rot13)", old_file)
+def get_new_file_name(old_file_name):
+    match = re.match(r"((.+)\.rot13)", old_file_name)
     if not match:
-        new_file = old_file + '.rot13'
-        return open(new_file, 'w')
-
+        return old_file_name + '.rot13'
     else:
-        new_file = old_file[0:-6]
-        return open(new_file, 'w')
+        return old_file_name[0:-6]
 
 
-def crypt_file(old_file, file):
+def read_file(file_name):
+    letters = []
+    file = open(file_name, 'r')
 
-    old_letters = []
-    old_file = open(sys.argv[1], 'r')
-
-    for l in old_file.read():
-        old_letters.append(l)
-
-    old_file.close()
-    file = create_new_file(sys.argv[1])
-
-    for letter in old_letters:
-        file.write(rot13(letter))
+    for l in file.read():
+        letters.append(l)
 
     file.close()
+    return letters
+
+
+def crypt_file(old_file, new_file):
+    new_file = open(new_file, 'w')
+
+    for letter in read_file(old_file):
+        new_file.write(rot13(letter))
+
+    new_file.close()
+
+
+def usage():
+    print('usage: ', sys.argv[0], ' <file_name>')
+    sys.exit(1)
+
+
+def check_overwrite(file_name):
+    if os.path.isfile(file_name):
+        erase = input(file_name + ' already exist. Do you want to erase it? (ny) ')
+        if erase == 'n':
+            sys.exit(1)
 
 
 def main():
+    args = sys.argv
 
-    if len(sys.argv) != 2 or not os.path.isfile(sys.argv[1]):
-        print('usage: ', sys.argv[0], ' <file_name>')
-        sys.exit(1)
+    if len(args) != 2 or not os.path.isfile(args[1]):
+        usage()
 
-    if os.path.isfile(sys.argv[1] + '.rot13') or os.path.isfile(sys.argv[1][0:-6]):
-        existing_file = input(sys.argv[1] + ' already have a crypted version. Do you want to erase it? (ny) ')
-        if existing_file == 'n':
-            sys.exit(1)
-        elif existing_file == 'y':
-            crypt_file(sys.argv[1], create_new_file(sys.argv[1]))
-
-    crypt_file(sys.argv[1], create_new_file(sys.argv[1]))
+    file_name = sys.argv[1]
+    output_file_name = get_new_file_name(file_name)
+    check_overwrite(output_file_name)
+    crypt_file(file_name, output_file_name)
 
 
 if __name__ == '__main__':
