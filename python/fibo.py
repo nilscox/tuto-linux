42d7fe1b0086d5457dc9158359e6986d9da62546
Bopzor <violaine.ballandin@gmail.com>
2017-12-02 15:05:32 +0100

    delete useless lines

--- python/fibo.py
+++ python/fibo.py
@@ -1,10 +1,9 @@
 def fibo(n):
     tab = [0, 1]
     x = 1
-    y = tab.index(x)
 
     while n > len(tab):
         tab.append(tab[x - 1] + tab[x])
-        x = x + (tab[y] + tab[y - 1])
+        x += 1
 
     return tab[:n]
