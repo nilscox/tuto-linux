ce91f27464109ea60c771628b1c1ac7357f066f6
Bopzor <violaine.ballandin@gmail.com>
2017-12-06 16:12:55 +0100

    change 'c' with 'key, value' to be more easy to read

--- python/dict-to-string/in_dict.py
+++ python/dict-to-string/in_dict.py
@@ -2,7 +2,7 @@ def in_dict(str):
     a = str.split('+')
     b = {}
     for i in a:
-        c = i.split('=')
-        b[c[0]] = c[1]
+        key, value = i.split('=')
+        b[key] = value
 
     return b
