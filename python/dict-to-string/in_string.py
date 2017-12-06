1ffcbff8c0eefad5698bbe5a785c25f328be5ef3
Bopzor <violaine.ballandin@gmail.com>
2017-12-06 17:10:43 +0100

    Use only dict.items() and join() for serialization

--- python/dict-to-string/in_string.py
+++ python/dict-to-string/in_string.py
@@ -1,8 +1,7 @@
 def in_string(dict):
-    tuples = [(x, dict[x]) for x in dict.keys()]
-    str = ""
-
-    for i in range(len(tuples)):
-            str += tuples[i][0] + "=" + tuples[i][1] + "+"
-
-    return str[:-1]
+    string = "="
+    l = ""
+    a = dict.items()
+    for i in a:
+        l += "+" + string.join(i)
+    return l[1:]
